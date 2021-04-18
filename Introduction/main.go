cdee04e26697e121224d1a64ce38bcff48979db7
Shreyal Shah <shreyalshah2014@gmail.com>
2021-04-18 03:14:48 -0400

    temp commit

--- Introduction/main.go
+++ Introduction/main.go
@@ -1,28 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+
+	"example.com/handlers"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-}
-
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World! ")
-		d, err := ioutil.ReadAll(r.Body)
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
 
-		if err != nil {
-			http.Error(w, "oooppps", http.StatusBadRequest)
-			return
-		}
-
-		fmt.Fprintf(w, "Hi there, %s", d)
-	})
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
